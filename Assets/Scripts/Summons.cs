173131444724efbac6cc02b494e5ab2a2bc76e62
mhtahir786 <158337919+mhtahir786@users.noreply.github.com>
2024-12-08 16:20:52 +0000

    Created Summon Animation & Blank Conclusion Canvas
    Summon animation created using free unity assets, and a conclusion canvas which will show character summoned

--- Assets/Scripts/Summons.cs
+++ Assets/Scripts/Summons.cs
@@ -1,17 +1,67 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class Summons : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // *** Variables
+    private const float kAnimationTime = 5.0f;  // animation time in seconds
+
+    [SerializeField]
+    private Canvas selectionCanvas;
+    [SerializeField]
+    private Canvas animationCanvas;
+    [SerializeField]
+    private Canvas conclusionCanvas;
+
+
+    // *** Functions
+    /// <summary>
+    /// Go through the motions of a summon; user selects, animation plays, conclusion shows.
+    /// </summary>
+    public void StartSummon()
     {
+        // Disable selection & display animation
+        selectionCanvas.gameObject.SetActive(false);
+        animationCanvas.gameObject.SetActive(true);
+
+        Debug.Log("Should show ANIMATION");  // REMOVE
+
+        // Simulate a characted pulled
+
         
+        // Show different animation based on character rarity
+
+
+        // After animation completion, show conclusion
+        Invoke(nameof(ShowConclusion), kAnimationTime);
+
+        Debug.Log("Should show CONCLUSION");  // REMOVE
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Called in the process of summoning after animation expires
+    /// </summary>
+    private void ShowConclusion()
     {
-        
+        // Change conclusion canvas background to character summoned
+
+        // Initiate a smooth transition animation (for conclusion background)
+
+        // Show conclusion canvas
+        animationCanvas.gameObject.SetActive(false);
+        conclusionCanvas.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Restart summons page by going to selection canvas
+    /// </summary>
+    public void RestartSummonsPage()
+    {
+        // Enable selection canvas, disabling the rest
+        conclusionCanvas.gameObject.SetActive(false);
+        selectionCanvas.gameObject.SetActive(true);
+
+        Debug.Log("Should show SELECTION");  // REMOVE
     }
 }
