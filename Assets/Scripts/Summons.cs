ed7bc6fbe3104a670e8953896e3db7d071baa121
mhtahir786 <158337919+mhtahir786@users.noreply.github.com>
2024-12-08 21:15:45 +0000

    Created Static Summon Functionality
    Generates from a set of pre-defined characters, waiting until characters are imported and a class for them exists to make it dynamic

--- Assets/Scripts/Summons.cs
+++ Assets/Scripts/Summons.cs
@@ -1,18 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Summons : MonoBehaviour
 {
     // *** Variables
     private const float kAnimationTime = 5.0f;  // animation time in seconds
+    private const float kMagicDelay = 1.0f;
+    private const float kLightningDelay = 3.0f;
+    //List<Characters> characterList;  // FUTURE
 
+    // Canvases
     [SerializeField]
     private Canvas selectionCanvas;
     [SerializeField]
     private Canvas animationCanvas;
     [SerializeField]
     private Canvas conclusionCanvas;
+
+    // Animation & Effects
+    [SerializeField]
+    private GameObject magicEffect;
+    [SerializeField]
+    private GameObject lightningEffect;
+
+    // Character Generation
+    [SerializeField]
+    private RawImage backgroundImage;
+    [SerializeField]
+    List<Sprite> characterList;  // remove once character class exists
 
 
     // *** Functions
@@ -25,32 +41,80 @@ public class Summons : MonoBehaviour
         selectionCanvas.gameObject.SetActive(false);
         animationCanvas.gameObject.SetActive(true);
 
-        Debug.Log("Should show ANIMATION");  // REMOVE
-
-        // Simulate a characted pulled
-
-        
-        // Show different animation based on character rarity
+        // Ensure user has enough currency
 
 
-        // After animation completion, show conclusion
-        Invoke(nameof(ShowConclusion), kAnimationTime);
+        // Generate a character to display
+        int characterNum = GenerateCharacter();
 
-        Debug.Log("Should show CONCLUSION");  // REMOVE
+        // Show conclusion - this function accounts for animation time
+        ShowConclusion(characterNum);
+    }
+
+    /// <summary>
+    /// Generate a character for the specified summon
+    /// </summary>
+    private int GenerateCharacter()
+    {
+        // - - IMPORTANT = currently static, will change when more characters class exists
+        // Generate a random number (will have biased rarities)
+        int randomNum = UnityEngine.Random.Range(0, characterList.Count);
+        Debug.Log("Random number: " + randomNum);  // REMOVE
+
+        // Modify animation based on character rarity
+        if (randomNum >= 3)  // silver rarity or gold
+        {
+            Invoke(nameof(PlayMagicEffect), kMagicDelay);
+            if (randomNum >= 5)  // gold rarity
+            {
+                Invoke(nameof(PlayLightningEffect), kLightningDelay);
+            }
+        }
+
+        // Return index of character generated
+        return randomNum;
+    }
+
+    /// <summary>
+    /// Called in generate character, plays sparkle effect which is initially disabled.
+    /// </summary>
+    private void PlayMagicEffect()
+    {
+        magicEffect.SetActive(true);
+    }
+
+    /// <summary>
+    /// Called in generate character, plays lightning effect which is initially disabled.
+    /// </summary>
+    private void PlayLightningEffect()
+    {
+        lightningEffect.SetActive(true);
     }
 
     /// <summary>
     /// Called in the process of summoning after animation expires
     /// </summary>
-    private void ShowConclusion()
+    private void ShowConclusion(int charNum)
     {
         // Change conclusion canvas background to character summoned
+        backgroundImage.texture = characterList[charNum].texture;
 
-        // Initiate a smooth transition animation (for conclusion background)
+        // Show conclusion canvas after animation time
+        Invoke(nameof(ShowConclusionHelper), kAnimationTime);
+    }
 
-        // Show conclusion canvas
+    /// <summary>
+    /// Helper function called in show conclusion to change canvas to conclusion - since Invoke can't take function parameters
+    /// </summary>
+    private void ShowConclusionHelper()
+    {
+        // Show conclusion and disable other canvases
         animationCanvas.gameObject.SetActive(false);
         conclusionCanvas.gameObject.SetActive(true);
+
+        // Disable effects
+        magicEffect.SetActive(false);
+        lightningEffect.SetActive(false);
     }
 
     /// <summary>
@@ -61,7 +125,5 @@ public class Summons : MonoBehaviour
         // Enable selection canvas, disabling the rest
         conclusionCanvas.gameObject.SetActive(false);
         selectionCanvas.gameObject.SetActive(true);
-
-        Debug.Log("Should show SELECTION");  // REMOVE
     }
 }
