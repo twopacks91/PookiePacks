7b90143e7105ec72929b4c114f2a4ff42c57ae4d
GwiggyOnTop <whisethis@gmail.com>
2024-12-11 02:43:28 +0000

    UpdateProfilePic
    Update should hopefully work

---
+++ Assets/Scripts/UpdateProfile.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Security.Permissions;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+using UnityEngine.SocialPlatforms.Impl;
+
+
+public class Profile : MonoBehaviour
+{
+    public PlayerData data;
+    public TMP_Text UsernameTxt;
+
+    public void Start()
+    {
+        data = new PlayerData();
+        UsernameTxt.text = "Username: " + data.GetUsername();
+    }
+
+    public static Profile Instance;
+
+
+    [Space]
+    [Header("Profile Picture Update Data")]
+    public GameObject profileUpdatePanel;
+    public Image profileImage;
+    public TMP_InputField urlInputField;
+
+    private void Awake()
+    {
+        CreateInstance();
+    }
+
+    private void CreateInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void OpenCloseProfileUpdatePanel()
+    {
+        profileUpdatePanel.SetActive(!profileUpdatePanel.activeSelf);
+    }
+
+
+
+    public void LoadProfileImage()
+    {
+        StartCoroutine((LoadProfileImageIE(urlInputField.text)));
+    }
+
+    public IEnumerator LoadProfileImageIE(string url)
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+        }
+        else
+        {
+            Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2());
+
+
+            profileImage.sprite = sprite;
+            profileUpdatePanel.SetActive(false);
+
+        }
+
+    }
+
+    
+}
